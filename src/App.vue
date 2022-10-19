01133fcb1b413bb809783dfed8e163343a758599
Lucia L <lucia@MacBook-Air-de-Lucia.local>
2022-10-19 19:27:02 +0200

    changes in css

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="flex flex-col m-16 p-6 border border-[#312a4fad] rounded-2xl gap-3 bg-[#504872]">
-    <div class="flex flex-col gap-2">
-      <h1 class="text-white">
-        KIBU Games
-      </h1>
-      <h5>Good games with tasty offers</h5>
+    <div class="flex flex-col-2 gap-2">
+      <div class="w-20 mt-8">
+        <img src="./img/logo.png">
+      </div>
+      <div class="flex flex-col-2">
+        <h1 class="text-white text-9xl ml-10">
+          KIBU
+        </h1>
+        <p class="text-xl mt-7 ml-4 font-bold text-[#fcfcfcd3]">
+          games
+        </p>
+      </div>
+      <div>
+        <h5>Good games with tasty offers</h5>
+      </div>
 
     </div>
     <router-view />
