ae1a0ad060d7e3b8d5935e8a8035e627a0150a0e
Lucia L <lucia@MacBook-Air-de-Lucia.local>
2022-10-19 18:09:24 +0200

    Adding css

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex flex-col m-16 p-6 border border-gray-900 rounded-2xl gap-3">
+  <div class="flex flex-col m-16 p-6 border border-[#312a4fad] rounded-2xl gap-3 bg-[#504872]">
     <div class="flex flex-col gap-2">
-      <h1 class="text-orange-700">
+      <h1 class="text-white">
         KIBU Games
       </h1>
       <h5>Good games with tasty offers</h5>
