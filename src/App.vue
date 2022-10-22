d63c289ad52313d0e8ac033c9f92b71ea08b5f84
gatzcat <77858048+gatzcat@users.noreply.github.com>
2022-10-22 17:54:07 +0200

    added loading animation, pagination still in progress

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,38 @@
 <template>
   <div>
-    <div class="flex flex-col mt-32 mx-32 px-16 border-8 border-[#645e7d4b] rounded-full gap-3 bg-[#504872]">
+
+    <!-- START: top horizontal row -->
+    <div class="flex flex-col mt-8 mx-8 px-10 border-4 border-[#645e7d7d] rounded-full gap-3 bg-[#504872]">
+      
       <div class="flex flex-col-3 gap-2">
+        
         <div class="w-20 mt-8 cursor-pointer">
           <img src="./img/logo.png">
         </div>
+
         <div class="flex flex-col-2 w-3/4">
+
+          <!-- START: test name text, mira a ver si te gusta, si no, quitalo" -->
+          <!-- <div id="test text logo group" class="flex flex-col relative w-1/5 items-center">
+            <h1 class="text-white text-9xl ">
+              KIBU
+            </h1>
+            <h1 class="text-3xl font-bold text-[#fcfcfcd3] absolute bottom-1">
+              games
+            </h1>
+          </div> -->
+          <!-- END: test name text, mira a ver si te gusta, si no, quitalo" -->
+
+          <!-- START original name text -->
           <h1 class="text-white text-9xl ml-10">
             KIBU
           </h1>
           <p class="text-xl mt-7 ml-4 font-bold text-[#fcfcfcd3]">
             games
           </p>
+          <!-- START original name text -->
         </div>
+        
         <div class="flex items-center opacity-75">
         <img src="./img/bookmark-solid.png" class="h-9 m-4 cursor-pointer">
         <img src="./img/bell-solid.png" class="h-9 m-4 cursor-pointer">
@@ -20,8 +40,9 @@
         </div>
     </div> 
     </div>
+    <!-- END: top horizontal row -->
     
-    <!-- no tocar router-view -->
+    <!-- no tocar router-view, es todo lo demás-->
     <router-view/>
   </div>
 </template>
