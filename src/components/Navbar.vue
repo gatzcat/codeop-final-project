c301bf56defb6b6139a37aefbb30f97a92bed243
Lucia L <lucia@MacBook-Air-de-Lucia.local>
2022-10-24 21:33:02 +0200

    Navbar tablet responsive

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,10 @@
 <template>
     
-    <div class="flex flex-col mt-8 mx-8 px-10 border-4 dark-purple rounded-full gap-3 border-purple">
+    <div class="flex flex-col mt-3 lg:mt-8 md:mx-8 md:px-10 md:border-4 dark-purple rounded-full border-purple">
       
       <div class="flex flex-col-3 gap-2">
         
-        <div class="w-20 mt-8 cursor-pointer">
+        <div class="w-16 mt-6 lg:w-20 lg:mt-8 cursor-pointer">
           <img src="../img/logovolume.png">
         </div>
 
@@ -22,10 +22,10 @@
           <!-- END: test name text, mira a ver si te gusta, si no, quitalo" -->
 
           <!-- START original name text -->
-          <h1 class="text-white text-9xl ml-10">
+          <h1 class="text-white text-8xl ml-6 lg:text-9xl lg:ml-10">
             KIBU
           </h1>
-          <p class="text-xl mt-7 ml-4 font-bold text-[#fcfcfcd3]">
+          <p class="text-l mt-4 ml-2 lg:text-xl lg:mt-7 lg:ml-4 font-bold text-[#fcfcfcd3]">
             games
           </p>
           <!-- START original name text -->
@@ -33,9 +33,9 @@
         
         <div class="flex items-center opacity-75">
             <!-- <CurrencySelector />     -->
-            <img src="../img/bookmark-solid.png" class="h-9 m-4 cursor-pointer">
-            <img src="../img/bell-solid.png" class="h-9 m-4 cursor-pointer">
-            <img src="../img/user-solid.png" class="h-9 m-4 cursor-pointer">
+            <img src="../img/bookmark-solid.png" class="h-6 m-4 lg:h-9 cursor-pointer">
+            <img src="../img/bell-solid.png" class="h-6 m-4 lg:h-9 cursor-pointer">
+            <img src="../img/user-solid.png" class="h-6 m-4 lg:h-9 cursor-pointer">
         </div>
     </div> 
     </div>
